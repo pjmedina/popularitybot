26a93664ede2fe96deae0e538a86f7689bad152e
Pia Medina <pjmedina@uwaterloo.ca>
2017-11-13 19:50:29 +0000

    Added test_pandas.py

---
+++ analysis/test_pandas.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class TestPandas(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestStorage, self).__init__(*args, **kwargs)
+
+    def test_pandas(self):
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
