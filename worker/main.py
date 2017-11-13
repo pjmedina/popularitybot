fc51563622d387a7e0677afb261ccb4313c96942
Quinn Turner <quinn.turner@edu.uwaterloo.ca>
2017-11-13 03:15:48 +0000

    Fix for casting a str as int.

--- worker/main.py
+++ worker/main.py
@@ -22,9 +22,9 @@ def main_args(argv):
         elif opt in ("-s", "--subreddit"):
             subreddit = arg
         elif opt in ("-p", "--post_count"):
-            post_count = arg
+            post_count = int(arg)
         elif opt in ("-l", "--limit"):
-            limit = arg
+            limit = int(arg)
     main(subreddit, post_count, limit)
 
 
