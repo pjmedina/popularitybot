5404670bfa2d2b7521c3223fa00e6532123b0510
Quinn Turner <quinn.turner@edu.uwaterloo.ca>
2017-11-13 21:38:44 -0500

    Attempt to get useful insights.

--- analysis/test_storage.py
+++ analysis/test_storage.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from storage import Storage
 
 
@@ -25,6 +26,17 @@ class TestStorage(unittest.TestCase):
         testdoc = self.db.test
         record = testdoc.find_one()
         self.assertEqual(record['test'], "abc123")
+
+    def test_for_deadline(self):
+        name_link = self.storage.user_collection.find({}, {"data.name": 1, "data.link_karma": 1})
+        name_score = self.storage.post_collection.find({}, {"data.author": 1, "data.score": 1})
+        name_link_revised = {}
+        for d in name_link:
+            name_link_revised[d['data.name']] = d['data.link_karma']
+        for a in name_score:
+            a['data.score'] = name_link_revised['data.name']
+        with open('abc.json', "w") as file_name:
+            json.dump(name_link_revised, file_name)
 
 
 if __name__ == '__main__':
