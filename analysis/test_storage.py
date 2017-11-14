a3352dda06f8814b2faeab934d17c0f02390a201
Quinn Turner <quinn.turner@edu.uwaterloo.ca>
2017-11-14 03:41:36 +0000

    analysis stuff.

--- analysis/test_storage.py
+++ analysis/test_storage.py
@@ -12,8 +12,8 @@ class TestStorage(unittest.TestCase):
         # rendering all tests useless. Must have the config file available.
         self.storage = Storage(config_file='storage_creds.ini',
                                config_header='database_info',
-                               new_collection_name='test_new_jsons',
-                               user_collection_name='test_user_jsons',
+                               new_collection_name='reddit_new_jsons',
+                               user_collection_name='reddit_user_jsons',
                                vision_collection_name='test_vision_info')
         self.client = self.storage.client
         self.db = self.storage.db
@@ -31,10 +31,12 @@ class TestStorage(unittest.TestCase):
         name_link = self.storage.user_collection.find({}, {"data.name": 1, "data.link_karma": 1})
         name_score = self.storage.post_collection.find({}, {"data.author": 1, "data.score": 1})
         name_link_revised = {}
-        for d in name_link:
-            name_link_revised[d['data.name']] = d['data.link_karma']
-        for a in name_score:
-            a['data.score'] = name_link_revised['data.name']
+        entries = name_link[:]
+        for d in entries:
+            name_link_revised[d['data']['name']] = {"karma": d['data']['link_karma'], "score": -1}
+        entriess = name_score[:]
+        for a in entriess:
+            name_link_revised[a['data']['author']]['score'] = a['data']['score']
         with open('abc.json', "w") as file_name:
             json.dump(name_link_revised, file_name)
 
